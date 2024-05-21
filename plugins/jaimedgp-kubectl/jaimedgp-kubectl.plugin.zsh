47a1e8188d7191f4233fb34bb22ce0e8a2df19b5
jaimedgp <jaimedgp@predictia.es>
2024-05-21 14:15:51 +0200

    Nex kubectl comands with fzf

--- plugins/jaimedgp-kubectl/jaimedgp-kubectl.plugin.zsh
+++ plugins/jaimedgp-kubectl/jaimedgp-kubectl.plugin.zsh
@@ -41,6 +41,40 @@ alias kgpa='kubectl get pods --all-namespaces'
 alias kpf="kubectl port-forward"  # Port forwarding
 alias kcp='kubectl cp'            # File copy
 
+function kc() {
+    context=$(kubectl config get-contexts | fzf | awk '{print $1}')
+    if [[ $context == "*" ]]; then
+        kubectl config current-context | awk '{print "You are currently using context: " $1}'
+    else
+        if [[ $context == "CURRENT" ]]; then
+            echo "Invalid context selected"
+            kubectl config current-context | awk '{print "Back to the currently using context: " $1}'
+        else
+            if [[ $context != "" ]]; then
+                kubectl config use-context $context
+            fi
+        fi
+    fi
+}
+
+function kl() {
+    pod=$(kubectl get pods | fzf | awk '{print $1}')
+    if [[ $pod != "" ]]; then
+        if [[ $pod != "NAME" ]]; then
+            kubectl logs $pod
+        fi
+    fi
+}
+
+function kex() {
+    pod=$(kubectl get pods | fzf | awk '{print $1}')
+    if [[ $pod != "" ]]; then
+        if [[ $pod != "NAME" ]]; then
+            kubectl exec -ti $pod -- /bin/bash
+        fi
+    fi
+}
+
 #---------------------------------------------------------------------------
 
 alias k3="sudo k3s kubectl"
@@ -69,3 +103,4 @@ alias k3gpa='sudo k3s kubectl get pods --all-namespaces'
 
 alias k3pf="sudo k3s ubectl port-forward"  # Port forwarding
 alias k3cp='sudo k3s ubectl cp'            # File copy
+
