3ec617eccebfb329a1de20c788f5a67bac99a0c6
jaimedgp <jaimedgp@predictia.es>
2024-10-10 11:13:21 +0200

    Add fzf find functions for git use

--- plugins/jaimedgp-git/jaimedgp-git.plugin.zsh
+++ plugins/jaimedgp-git/jaimedgp-git.plugin.zsh
@@ -4,5 +4,29 @@ alias gd="git diff"
 alias gc="git commit -m"
 alias gp="git push -u origin"
 alias gr="git restore"
-alias gch="git checkout"
+alias gchck="git checkout"
 alias gnb="git checkout -b"
+
+
+function gchbf() {
+    branch=$(git branch | fzf --height 40% --reverse | awk '{print $1}')
+    if [[ $branch != "" ]]; then
+        if [[ $branch != "NAME" ]]; then
+            git checkout $branch
+        fi
+    fi
+}
+
+function gdf() {
+    file=$(git status --porcelain | grep '^ M' | sed s/^...// | fzf --height 40% --reverse)
+    if [[ $file != "" ]]; then
+        git diff $file
+    fi
+}
+
+function grf() {
+    file=$(git status --porcelain | grep '^ M' | sed s/^...// | fzf --height 40% --reverse)
+    if [[ $file != "" ]]; then
+        git restore $file
+    fi
+}
