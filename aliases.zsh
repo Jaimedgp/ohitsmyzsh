7e158d10ad66923c21d6729d34ca4372a2f1eb33
jaimedgp <jaimedgp@predictia.es>
2024-10-30 09:19:59 +0100

    List files using 'eza' command instead of 'ls' or 'tree'

--- aliases.zsh
+++ aliases.zsh
@@ -1,9 +1,15 @@
 alias 0='cd $HOME'
-alias t='tree'
-alias t1='tree -L 1'
-alias t2='tree -L 2'
-alias t3='tree -L 3'
-alias t4='tree -L 4'
+alias t='eza --icons=always --color=always --no-permissions --no-time --no-user --no-filesize --group-directories-first --tree'
+alias t1='eza --icons=always --color=always --no-permissions --no-time --no-user --no-filesize --group-directories-first --tree --level=1'
+alias t2='eza --icons=always --color=always --no-permissions --no-time --no-user --no-filesize --group-directories-first --tree --level=2'
+alias t3='eza --icons=always --color=always --no-permissions --no-time --no-user --no-filesize --group-directories-first --tree --level=3'
+alias t4='eza --icons=always --color=always --no-permissions --no-time --no-user --no-filesize --group-directories-first --tree --level=4'
+
+# alias t='tree'
+# alias t1='tree -L 1'
+# alias t2='tree -L 2'
+# alias t3='tree -L 3'
+# alias t4='tree -L 4'
 
 function lc() {
     echo "-- $(ls $1 | wc -l) elements"
@@ -22,10 +28,16 @@ function llac() {
     /bin/ls -Al --color=auto $1
 }
 
-alias la='ls -A'
-alias ll='ls -l'
-alias lla='ls -Al'
-alias l='ls -C'
+
+alias ls='eza --icons=always --color=always --no-permissions --no-time --no-user --no-filesize --grid --width 150 --group-directories-first'
+alias ls='eza --icons=always --color=always --no-permissions --no-time --no-user --no-filesize --grid --width 150 --group-directories-first'
+alias la='eza --icons=always --color=always --no-permissions --no-time --no-user --no-filesize --grid --width 150 --group-directories-first --all'
+alias ll='eza --icons=always --color=always --long --no-filesize --group-directories-first'
+alias lla='eza --icons=always --color=always --long --no-filesize --all --group-directories-first'
+# alias la='ls -A'
+# alias ll='ls -l'
+# alias lla='ls -Al'
+# alias l='ls -C'
 
 alias ':q'="exit"
 
